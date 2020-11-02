cf646316e60e72fa3ced4e7514077b49607ec02f
YBA-IBM <47538942+YBA-IBM@users.noreply.github.com>
2020-11-01 23:20:10 -0500

    Tests for management tag
    Signed-off-by: YBA-IBM <yoel.ben-avraham@ibm.com>

--- test.sh
+++ test.sh
@@ -8,8 +8,15 @@ export ANSI_RESET="\e[0m"
 
 echo -e "\n $ANSI_YELLOW *** FUNCTIONAL TEST(S) *** $ANSI_RESET \n"
 
-echo -e "$ANSI_YELLOW Proper Testing TBI: $ANSI_RESET"
-
-
+echo -e "$ANSI_YELLOW Testing Management Console Functionality: $ANSI_RESET"
+docker run -d --hostname my-rabbit --name some-rabbit quay.io/ibmz/rabbitmq:3.8.9-management
+curl http://localhost:8080 
+docker stop some-rabbit && docker rm some-rabbit
+docker run -d --hostname my-rabbit --name some-rabbit -p 8080:15672 quay.io/ibmz/rabbitmq:3.8.9-management
+curl http://localhost:8080 
+docker stop some-rabbit && docker rm some-rabbit
+docker run -d --hostname my-rabbit --name some-rabbit -p 8080:15672 -e RABBITMQ_DEFAULT_USER=user -e RABBITMQ_DEFAULT_PASS=password -e RABBITMQ_DEFAULT_VHOST=my_vhost quay.io/ibmz/rabbitmq:3.8.9-management
+curl http://localhost:8080 
+docker stop some-rabbit && docker rm some-rabbit
 
 echo -e "\n $ANSI_GREEN *** FUNCTIONAL TEST(S) COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
