26f25de8fec727425c224645ee56378b49fcf9b0
YBA-IBM <47538942+YBA-IBM@users.noreply.github.com>
2020-11-01 21:48:19 -0500

    Remove Operation Calls
    Signed-off-by: YBA-IBM <yoel.ben-avraham@ibm.com>

--- test.sh
+++ test.sh
@@ -15,9 +15,9 @@ docker stop some-rabbit && docker rm some-rabbit
 docker network create some-network
 docker run -d --hostname some-rabbit --name some-rabbit --network some-network -e RABBITMQ_ERLANG_COOKIE='secret cookie here' quay.io/ibmz/rabbitmq:3.8.9
 echo "Debug 0"
-docker run -i --rm --network some-network -e RABBITMQ_ERLANG_COOKIE='secret cookie here' quay.io/ibmz/rabbitmq:3.8.9 rabbitmqctl -n rabbit@some-rabbit list_users
+docker run -i --rm --network some-network -e RABBITMQ_ERLANG_COOKIE='secret cookie here' quay.io/ibmz/rabbitmq:3.8.9
 echo "Debug 1"
-docker run -i --rm --network some-network -e RABBITMQ_ERLANG_COOKIE='secret cookie here' -e RABBITMQ_NODENAME=rabbit@some-rabbit quay.io/ibmz/rabbitmq:3.8.9 rabbitmqctl list_users
+docker run -i --rm --network some-network -e RABBITMQ_ERLANG_COOKIE='secret cookie here' -e RABBITMQ_NODENAME=rabbit@some-rabbit quay.io/ibmz/rabbitmq:3.8.9
 echo "Debug 2"
 
 echo -e "\n $ANSI_GREEN *** FUNCTIONAL TEST(S) COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
